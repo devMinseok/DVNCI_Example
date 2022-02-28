9a161031c4684f2ad83c55b5f302318329073641
KangMinseok <qbq5000@gmail.com>
2022-02-28 17:50:04 +0900

    💄 RepositorySearch 화면 UI 구성
    + Use FlexLayout with PinLayout

--- DVNCI_Example/RepositorySerach/RepositorySearchView.swift
+++ DVNCI_Example/RepositorySerach/RepositorySearchView.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 
+import FlexLayout
+import PinLayout
+
 // MARK: - RepositorySearchViewProtocol
 protocol RepositorySearchViewProtocol {
     var controller: RepositorySearchControllerProtocol? { get set }
@@ -17,6 +20,63 @@ final class RepositorySearchView: UIView {
 
 // MARK: - Properties
     weak var controller: RepositorySearchControllerProtocol?
+    
+    fileprivate let rootFlexContainer = UIView()
+    
+    var searchField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+    
+    var searchButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Search", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        return button
+    }()
+    
+    var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = .lightGray
+        return tableView
+    }()
+    
+    init() {
+        super.init(frame: .zero)
+        
+        backgroundColor = .white
+        addSubview(rootFlexContainer)
+        
+        rootFlexContainer.flex
+            .direction(.column)
+            .paddingHorizontal(20)
+            .define { flex in
+                flex.addItem()
+                    .direction(.row)
+                    .paddingVertical(20)
+                    .define { flex in
+                        flex.addItem(searchField)
+                            .grow(9)
+                        flex.addItem(searchButton)
+                            .grow(1)
+                    }
+                
+                flex.addItem(tableView)
+                    .grow(10)
+            }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        rootFlexContainer.pin.all(pin.safeArea)
+        rootFlexContainer.flex.layout()
+    }
 }
 
 // MARK: - RepositorySearchViewProtocol Methods
